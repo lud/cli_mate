5f167cabc1d474fdcc471989df9d9e431c2d6fd4
lud <ludovic@demblans.com>
2025-09-16 23:37:19 +0200

    feat: Compress output of --help

--- lib/cli_mate/cli/usage_format/option_formatter/plain_text.ex
+++ lib/cli_mate/cli/usage_format/option_formatter/plain_text.ex
@@ -9,13 +9,21 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
   @arg_indent 2
   @max_doc_width 87
 
-  defp indent(n), do: List.duplicate(" ", n)
+  defp indent(n), do: List.duplicate(32, n)
 
   @impl true
   def format_head(title, docs, fmt_opts) do
     case docs do
-      nil -> format_heading(title, fmt_opts)
-      _ -> format_section(title, docs, fmt_opts)
+      nil -> format_command(title, fmt_opts)
+      _ -> [format_command(title, fmt_opts), "\n\n", docs]
+    end
+  end
+
+  defp format_command(title, fmt_opts) do
+    if Keyword.get(fmt_opts, :ansi_enabled, false) do
+      bright(title)
+    else
+      title
     end
   end
 
@@ -35,7 +43,7 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
 
   defp format_heading(title, fmt_opts) do
     if Keyword.get(fmt_opts, :ansi_enabled, false) do
-      bright(title)
+      ubright(title)
     else
       title
     end
@@ -50,22 +58,26 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
     ansi_enabled? = Keyword.get(fmt_opts, :ansi_enabled, false)
 
     doc_width = min(columns - @doc_indent, @max_doc_width)
-    doc_padding = [?\n, indent(@doc_indent)]
 
     Enum.map_intersperse(command.arguments, "\n\n", fn arg ->
-      title = arg_title(arg, ansi_enabled?)
+      name = Atom.to_string(arg.key)
+      title = arg_title(name, arg.repeat, ansi_enabled?)
+      len = String.length(name) + @arg_indent + if arg.repeat, do: 3, else: 0
+      spacing = @doc_indent - len
+      doc_padding = [?\n, indent(@doc_indent)]
+      first_padding = if spacing > 1, do: indent(spacing), else: doc_padding
 
-      case format_doc(arg, doc_width, doc_padding) do
+      case format_doc(arg, doc_width, first_padding, doc_padding) do
         :no_doc -> [indent(@arg_indent), title]
         doc -> [indent(@arg_indent), title, doc]
       end
     end)
   end
 
-  defp arg_title(arg, ansi_enabled?) do
-    title = if ansi_enabled?, do: bright(Atom.to_string(arg.key)), else: Atom.to_string(arg.key)
+  defp arg_title(name, repeat?, ansi_enabled?) do
+    title = if ansi_enabled?, do: bright(name), else: name
 
-    if arg.repeat do
+    if repeat? do
       [title, "..."]
     else
       title
@@ -76,47 +88,44 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
   def format_options(command, fmt_opts) do
     columns = Keyword.get_lazy(fmt_opts, :io_columns, &io_columns/0)
     ansi_enabled? = Keyword.get(fmt_opts, :ansi_enabled, false)
-
     options = command.options
-    has_short_opts? = Enum.any?(options, fn {_, %{short: short}} -> short != nil end)
-
-    signatures = Enum.map(options, &signature(&1, ansi_enabled?, has_short_opts?))
-
     doc_padding = [?\n, indent(@doc_indent)]
-
     doc_width = min(columns - @doc_indent, @max_doc_width)
 
-    docs = Enum.map(options, &format_doc(&1, doc_width, doc_padding))
+    docs =
+      Enum.map(options, fn opt ->
+        {signature, raw_len} = signature(opt, ansi_enabled?)
+        spacing = @doc_indent - (raw_len + @arg_indent)
+        first_padding = if spacing > 1, do: indent(spacing), else: doc_padding
+        doc = format_doc(opt, doc_width, first_padding, doc_padding)
 
-    opts_docs =
-      Enum.zip_with(signatures, docs, fn
-        signature, :no_doc ->
-          [indent(@arg_indent), signature, ?\n]
-
-        signature, doc ->
-          [indent(@arg_indent), signature, doc, ?\n]
+        case doc do
+          :no_doc -> [indent(@arg_indent), signature, ?\n]
+          doc -> [indent(@arg_indent), signature, doc, ?\n]
+        end
       end)
 
-    Enum.intersperse(opts_docs, ?\n)
+    Enum.intersperse(docs, ?\n)
   end
 
-  defp signature({_, option}, ansi_enabled?, shorts?) do
-    iodata_short = short_signature(option, ansi_enabled?, shorts?)
-    iodata_long = long_signature(option, ansi_enabled?)
-    [iodata_short, iodata_long]
+  defp signature({_, option}, ansi_enabled?) do
+    {iodata_short, short_len} =
+      case short_signature(option, ansi_enabled?) do
+        [] -> {[], 0}
+        short -> {short, 4}
+      end
+
+    {iodata_long, len} = long_signature(option, ansi_enabled?)
+    {[iodata_short, iodata_long], len + short_len}
   end
 
-  defp short_signature(option, ansi_enabled?, shorts?) do
+  defp short_signature(option, ansi_enabled?) do
     %Option{short: short} = option
 
-    if shorts? do
-      case short do
-        nil -> []
-        s when ansi_enabled? -> [bright(["-", Atom.to_string(s)]), ", "]
-        s -> ["-", Atom.to_string(s), ", "]
-      end
-    else
-      []
+    case short do
+      nil -> []
+      s when ansi_enabled? -> [bright(["-", Atom.to_string(s)]), ", "]
+      s -> ["-", Atom.to_string(s), ", "]
     end
   end
 
@@ -124,35 +133,34 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
     %Option{type: type} = option
     name = name(option)
 
-    signature =
+    len = String.length(name) + 2
+
+    {signature, len} =
       case type do
         t when t in [:boolean, :count] and ansi_enabled? ->
-          [bright(["--", name])]
+          {[bright(["--", name])], len}
 
         t when t in [:boolean, :count] ->
-          ["--", name]
+          {["--", name], len}
 
         _ when ansi_enabled? ->
           doc_arg = option.doc_arg
-          [bright(["--", name]), cyan([" <", doc_arg, ">"])]
+          {[bright(["--", name]), cyan([" <", doc_arg, ">"])], len + String.length(doc_arg) + 3}
 
         _ ->
           doc_arg = option.doc_arg
-          ["--", name, " <", doc_arg, ">"]
+          {["--", name, " <", doc_arg, ">"], len + String.length(doc_arg) + 3}
       end
 
-    if type == :count || option.keep do
-      repeat_opt(signature)
-    else
-      signature
-    end
+    {_formatted, _len} =
+      if type == :count || option.keep do
+        {[signature, " [...]"], len + 6}
+      else
+        {signature, len}
+      end
   end
 
-  defp repeat_opt(signature) do
-    [signature, " [...]"]
-  end
-
-  defp format_doc(%Argument{} = argument, width, pad_text) do
+  defp format_doc(%Argument{} = argument, width, first_padding, other_padding) do
     %{doc: doc} = argument
 
     case doc do
@@ -160,14 +168,17 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
         :no_doc
 
       _ ->
-        doc
-        |> unwrap_doc()
-        |> wrap_doc(width)
-        |> Enum.map(&[pad_text, &1])
+        doc =
+          doc
+          |> unwrap_doc()
+          |> wrap_doc(width)
+          |> Enum.intersperse(other_padding)
+
+        [first_padding, doc]
     end
   end
 
-  defp format_doc({_, %Option{} = option}, width, pad_text) do
+  defp format_doc({_, %Option{} = option}, width, first_padding, other_padding) do
     %Option{
       key: k,
       doc: doc,
@@ -187,10 +198,13 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
         :no_doc
 
       _ ->
-        doc
-        |> unwrap_doc()
-        |> wrap_doc(width)
-        |> Enum.map(&[pad_text, &1])
+        doc =
+          doc
+          |> unwrap_doc()
+          |> wrap_doc(width)
+          |> Enum.intersperse(other_padding)
+
+        [first_padding, doc]
     end
   end
 
@@ -206,6 +220,7 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
   end
 
   defp bright(iodata), do: [IO.ANSI.bright(), iodata, IO.ANSI.reset()]
+  defp ubright(iodata), do: [IO.ANSI.underline(), IO.ANSI.bright(), iodata, IO.ANSI.reset()]
   defp cyan(iodata), do: [IO.ANSI.cyan(), iodata, IO.ANSI.reset()]
 
   defp unwrap_doc(doc) do
@@ -231,9 +246,16 @@ defmodule CliMate.CLI.UsageFormat.OptionFormatter.PlainText do
       end
     end)
     |> case do
-      {_, [], lines} -> :lists.reverse(lines)
-      {_, current, lines} -> :lists.reverse([:lists.reverse(current) | lines])
+      {_, [], lines} -> lines
+      {_, current, lines} -> [:lists.reverse(current) | lines]
     end
+    # Fancy stuff. We need to reverse the lines but also map them so if one line
+    # starts with a backtick we insert a backspace before so the text aligns
+    # more naturally.
+    |> List.foldl([], fn
+      ["`" <> _ = h | t], acc -> [["\b", h | t] | acc]
+      line, acc -> [line | acc]
+    end)
   end
 
   defp format_default(_k, _value, default_doc) when is_binary(default_doc) do
